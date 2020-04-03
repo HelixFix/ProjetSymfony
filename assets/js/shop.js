745898eee170e75b9889839ecd5686ef94868cf5
HelixFix <jerome.rinner@gmail.com>
2020-04-03 17:36:05 +0200

    ressources pour l'API

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -16,27 +16,47 @@ console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 
 import Vue from 'vue'
 import Shop from './Shop.vue'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://api.insee.fr/entreprises/sirene/V3/siret?q';
+axios.defaults.headers.common['Authorization'] = "Bearer a6452ce6-b2d3-3292-bd24-c5de16d6d937";
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+axios.get('https://api.insee.fr/entreprises/sirene/V3/')
+  .then((response) => {
+    console.log(response.data);
+    console.log(response.status);
+    console.log(response.statusText);
+    console.log(response.headers);
+    console.log(response.config);
+  });
 
 
+  //
+  const AuthStr = 'Bearer '.concat(USER_TOKEN);
+  axios.get(URL, { headers: { Authorization: AuthStr } }).then(response => {
+          // If request is good...
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log('error 3 ' + error);
+        });
 
 // new Vue({
 //     el: "#shop",
 //     components: {ApiShop}
 // })
 
-new Vue({
-    el: "#donation",
-    components: {Donation},
-    template: "<Donation/>"
-    
-})
-
-try{
-    $response = new Response();
-    $response->headers->set('Authorization', 'Bearer 526c1976-308d-3df0-957a-416622680356');
-    $response->send();
-    return $this->redirect('https://api.insee.fr/entreprises/sirene/V3/siret?q=trancheEffectifsEtablissement:21');      
-}
-catch(\Exception $e){
-    error_log($e->getMessage());
-}
+// new Vue({
+//     el: '#shop',
+//     data () {
+//       return {
+//         info: null
+//       }
+//     },
+//     mounted () {
+//       axios
+//         .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+//         .then(response => (this.info = response))
+//     }
+//   })
