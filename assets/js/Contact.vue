7ed33ff73e9750f8853a14ba345c09737fb25252
HelixFix <jerome.rinner@gmail.com>
2020-04-03 16:27:15 +0200

    fix

--- assets/js/Contact.vue
+++ assets/js/Contact.vue
@@ -4,3 +4,15 @@
         <button @click="counter++"> Click me {{ counter }}</button>
     </div>
 </template>
+
+<script>
+export default {
+    data: () => ({
+        counter: 0
+})
+}
+</script>
+
+<style>
+
+</style>
